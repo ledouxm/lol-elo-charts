c14db300069331a0be9a6c58c3303ff6bd019344
Alexandre Stahmer <alexandre.stahmer@gmail.com>
2021-08-27 16:22:35 +0200

    chore: rm useWhisperHistory

--- packages/client/src/chat/ChatForm.tsx
+++ packages/client/src/chat/ChatForm.tsx
@@ -1,13 +1,10 @@
 import { TextInput } from "@/components/TextInput";
 import { getStateValuePath } from "@/functions/xstate";
-import { useArrayCursor } from "@/hooks/useArrayCursor";
 import { Button, Stack, useColorModeValue } from "@chakra-ui/react";
-import { on, WithOnSubmit } from "@pastable/core";
+import { WithOnSubmit } from "@pastable/core";
 import { useMachine } from "@xstate/react";
-import { useAtomValue } from "jotai/utils";
-import { ChangeEvent, FormEvent, MutableRefObject, useCallback, useEffect, useRef } from "react";
-import { msgsRefAtom } from "./Chat";
-import { extractCommandState, getChatFormMachine } from "./chatFormMachine";
+import { ChangeEvent, FormEvent, MutableRefObject, useRef } from "react";
+import { getChatFormMachine } from "./chatFormMachine";
 import { ChatSuggestionsProvider } from "./ChatSuggestionsProvider";
 
 export const ChatForm = ({ onSubmit: onSubmitProp, usernameInputRef }: ChatFormProps) => {
@@ -29,9 +26,6 @@ export const ChatForm = ({ onSubmit: onSubmitProp, usernameInputRef }: ChatFormP
     // Used to filter the suggestion list
     const value = state.context.value;
     const focusInput = () => usernameInputRef.current?.focus();
-
-    // Used to loop through whisper history with up/down keys
-    // const historyCursor = useWhisperHistory(usernameInputRef.current, !showSuggestions);
 
     const commandListBtnRef = useRef<HTMLButtonElement>();
     const chatContextProps = {
@@ -70,71 +64,4 @@ export const ChatForm = ({ onSubmit: onSubmitProp, usernameInputRef }: ChatFormP
 export type ChatFormValues = { msg: string };
 interface ChatFormProps extends WithOnSubmit<ChatFormValues, boolean> {
     usernameInputRef: MutableRefObject<HTMLInputElement>;
-}
-
-const hasFilledCommandSecondParam = (value: string) => extractCommandState(value).hasFilledSecondParam;
-
-function useWhisperHistory(inputElement: HTMLInputElement, enabled: boolean) {
-    const msgsRef = useAtomValue(msgsRefAtom);
-
-    // Get unique "to" usernames from sent whispers
-    const sentTo = Array.from(new Set(msgsRef.current.map((chat) => chat.self && chat.to).filter(Boolean)));
-
-    const [index, cursor] = useArrayCursor(sentTo.length);
-
-    // Loop through whisper history with up/down, reset input when index === -1
-    const openWhisperHistoryWithArrowKeys = useCallback(
-        (event: KeyboardEvent) => {
-            // No messages sent yet or history disabled
-            if (!sentTo.length || !enabled) return;
-
-            // Only allow looping through history if message is a `/w [username]` without message yet
-            if (inputElement.value) {
-                if (inputElement.value.startsWith("/w ")) {
-                    if (hasFilledCommandSecondParam(inputElement.value)) {
-                        return;
-                    }
-                } else {
-                    return;
-                }
-            }
-
-            if (!(event.key === "ArrowUp" || event.key === "ArrowDown")) {
-                return;
-            }
-
-            event.preventDefault();
-            if (event.key === "ArrowUp") {
-                index === sentTo.length - 1 ? cursor.reset() : cursor.next();
-                return;
-            }
-
-            index === 0 ? cursor.reset() : cursor.prev();
-        },
-        [inputElement, sentTo, cursor, index, enabled]
-    );
-
-    // openWhisperHistoryWithArrowKeys
-    useEffect(() => {
-        if (!inputElement) return;
-
-        return on(inputElement, "keydown", openWhisperHistoryWithArrowKeys);
-    }, [inputElement, openWhisperHistoryWithArrowKeys]);
-
-    // Update input value with `/w ${username}` according to array cursor index
-    useEffect(() => {
-        if (!inputElement || !sentTo.length) return;
-        if (!inputElement.value.startsWith("/w ")) return;
-
-        // Reset input on -1
-        if (index === -1) {
-            inputElement.value = "";
-            return;
-        }
-
-        const activeTo = sentTo[index];
-        inputElement.value = `/w ${activeTo} `;
-    }, [inputElement, sentTo, index]);
-
-    return cursor;
 }
