a927d7c4e8d4f4fd598a70d1aa32381cace4d70f
Alexandre Stahmer <alexandre.stahmer@gmail.com>
2021-09-08 17:39:32 +0200

    feat: isRegisterFormAtom

--- packages/client/src/components/LoginForm.tsx
+++ packages/client/src/components/LoginForm.tsx
@@ -3,7 +3,8 @@ import { onAxiosError, successToast } from "@/functions/toasts";
 import { getRandomColor, makeId } from "@/functions/utils";
 import { persistLocalPresence, persistRoles } from "@/hooks/usePresence";
 import { Button, Center, Flex, Stack } from "@chakra-ui/react";
-import { getRandomIntIn } from "@pastable/core";
+import { atomWithToggleAndStorage, getRandomIntIn } from "@pastable/core";
+import { useAtom } from "jotai";
 import { useEffect } from "react";
 import { useForm } from "react-hook-form";
 import { useMutation } from "react-query";
@@ -14,9 +15,16 @@ import { TextInput } from "./TextInput";
 const login = (values: LoginFormValues) => api.post("/auth/login", values).then((res) => res.data);
 const createUser = (values: LoginFormValues) => api.post("/auth/register", values).then((res) => res.data);
 
+const isRegisterFormAtom = atomWithToggleAndStorage("wss/isRegisterForm");
+
 export const LoginForm = () => {
+    const [isRegisterForm, setIsRegisterForm] = useAtom(isRegisterFormAtom);
     const { register, handleSubmit, getValues, setValue, watch } = useForm({
-        defaultValues: { name: "Guest-" + getRandomIntIn(0, 1000), password: "", type: "login", asGuest: true },
+        defaultValues: {
+            name: "Guest-" + getRandomIntIn(0, 1000),
+            password: "",
+            type: isRegisterForm ? "register" : "login",
+        },
     });
     const setType = (type: "login" | "create") => setValue("type", type);
 
@@ -54,7 +62,7 @@ export const LoginForm = () => {
     const onSubmit = (values: LoginFormValues) =>
         getValues("type") === "login" ? loginMutation.mutate(values) : createMutation.mutate(values);
 
-    const isAsGuest = watch("asGuest");
+    const isLoginForm = watch("type") === "login";
 
     useEffect(() => {
         const token = getAccessToken();
@@ -67,22 +75,26 @@ export const LoginForm = () => {
         <Stack as="form" onSubmit={handleSubmit(onSubmit)} spacing={4}>
             <Stack direction="row">
                 <TextInput label="Username" {...register("name", { required: true })} />
-                {!isAsGuest && <TextInput label="Password" type="password" {...register("password")} />}
+                {!isLoginForm && <TextInput label="Password" type="password" {...register("password")} />}
             </Stack>
             <Center>
                 <SwitchInput
-                    label={isAsGuest ? "Without account" : "As user"}
+                    label={isLoginForm ? "Without account" : "As user"}
                     colorScheme="red"
-                    {...register("asGuest")}
+                    defaultChecked={!isRegisterForm}
+                    onChange={(event) => {
+                        setIsRegisterForm(!event.target.checked);
+                        setValue("type", event.target.checked ? "login" : "register");
+                    }}
                     wrapperProps={{ w: "auto" }}
                 />
             </Center>
-            <Flex direction="row-reverse" justifyContent={isAsGuest ? "center" : "space-around"}>
+            <Flex direction="row-reverse" justifyContent={isLoginForm ? "center" : "space-around"}>
                 <Button type="submit" onClick={() => setType("login")} colorScheme="twitter">
                     Login
                 </Button>
                 {/* Reversing the order of btns so that the first one (Login) will get triggered on ENTER key submit */}
-                {!isAsGuest && (
+                {!isLoginForm && (
                     <Button type="submit" onClick={() => setType("create")}>
                         Register
                     </Button>
