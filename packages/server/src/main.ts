7cfe56324e1422fe21214b875da06872a826b62d
ledouxm <etudiant@mledoux.fr>
2022-08-09 23:42:15 +0200

    feat: use dotenv

--- packages/server/src/main.ts
+++ packages/server/src/main.ts
@@ -1,5 +1,5 @@
-import "./envVars";
-
+import dotenv from "dotenv";
+dotenv.config();
 import { makeApp } from "./app";
 
 const port = Number(process.env.HTTP_PORT) || 1337;
@@ -8,8 +8,7 @@ const start = async () => {
     const app = await makeApp();
     try {
         const address = await app.listen(port);
-        app.log.info(`server listening on ${address}`);
-        console.log({ port });
+        console.log(`server listening on ${address}`);
     } catch (err) {
         app.log.error(err);
         process.exit(1);
