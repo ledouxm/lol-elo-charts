6e47c7f081183aa0b2fc917b09abf0d309ca9b19
ledouxm <etudiant@mledoux.fr>
2022-08-10 16:10:20 +0200

    set host 0.0.0.0

--- packages/server/src/main.ts
+++ packages/server/src/main.ts
@@ -5,12 +5,12 @@ import { makeApp } from "./app";
 const port = Number(process.env.HTTP_PORT) || 1337;
 
 const start = async () => {
-    const app = await makeApp();
     try {
-        const address = await app.listen({ port });
+        const app = await makeApp();
+        const address = await app.listen({ port, host: "0.0.0.0" });
         console.log(`server listening on ${address}`);
     } catch (err) {
-        app.log.error(err);
+        console.log(err);
         process.exit(1);
     }
 };
