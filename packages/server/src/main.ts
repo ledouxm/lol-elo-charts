4d449788cb7c853cb045ddcb5e45663f7fd1a534
ledouxm <etudiant@mledoux.fr>
2022-08-10 17:16:12 +0200

    try using express

--- packages/server/src/main.ts
+++ packages/server/src/main.ts
@@ -1,14 +1,18 @@
 import dotenv from "dotenv";
 dotenv.config();
-import { makeApp } from "./app";
-
+// import { makeApp } from "./app";
+import express from "express";
 const port = Number(process.env.HTTP_PORT) || 1337;
 
 const start = async () => {
     try {
-        const app = await makeApp();
-        const address = await app.listen({ port });
-        console.log(`server listening on ${address}`);
+        const app = express();
+        app.use("*", (_, res) => res.send("ok"));
+        app.listen(port, () => {
+            console.log(`server listening on ${port}`);
+        });
+        // const app = await makeApp();
+        // const address = await app.listen({ port });
     } catch (err) {
         console.log(err);
         process.exit(1);
