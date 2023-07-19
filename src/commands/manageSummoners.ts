a81ffbc062a8676780c653790da593db28266902
ledouxm <mledoux@mledoux.fr>
2023-07-19 03:25:18 +0200

    feat: add listSummoners command

--- src/commands/manageSummoners.ts
+++ src/commands/manageSummoners.ts
@@ -1,6 +1,10 @@
 import { ApplicationCommandOptionType, CommandInteraction } from "discord.js";
 import { Discord, Slash, SlashOption } from "discordx";
 import { addSummoner, removeSummoner } from "../routes";
+import { db } from "../db/db";
+import { summoner } from "src/db/schema";
+import { eq } from "drizzle-orm";
+import { EmbedBuilder } from "@discordjs/builders";
 
 @Discord()
 export class ManageSummoner {
@@ -34,5 +38,16 @@ export class ManageSummoner {
         console.log("removeSummoner");
         await removeSummoner(name, interaction.channelId);
         interaction.reply("Removed summoner " + name);
+    }
+
+    @Slash({ name: "listsummoners", description: "List all summoners being tracked" })
+    async listSummoners(interaction: CommandInteraction) {
+        const summoners = await db.select().from(summoner).where(eq(summoner.channelId, interaction.channelId));
+
+        const embed = new EmbedBuilder()
+            .setTitle("Summoner tracked on this channel")
+            .setDescription(summoners.map((s) => s.currentName).join("\n"));
+
+        interaction.reply({ embeds: [embed] });
     }
 }
