6cbdff3757170f8d00072c04d1d174ec5ce10ce0
ledouxm <martin.ledoux.pro@gmail.com>
2024-03-06 20:26:07 +0100

    add logs

--- packages/core/src/features/details/browser.ts
+++ packages/core/src/features/details/browser.ts
@@ -39,6 +39,8 @@ export const getScreenshotBuffer = async ({
 }) => {
     const page = await getPage();
 
+    console.log(clip);
+
     await page.setViewport(clip);
     await page.setContent(html);
 
