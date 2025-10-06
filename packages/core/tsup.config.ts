c42a136df25c9d44b2c53368d0a46228279327f1
ledouxm <mledoux@mledoux.fr>
2025-10-06 19:21:25 +0200

    fix: core build

--- packages/core/tsup.config.ts
+++ packages/core/tsup.config.ts
@@ -2,5 +2,5 @@ import { defineConfig } from "tsup";
 export default defineConfig({
     entry: ["src/main.tsx"],
     format: ["esm"],
-    noExternal: ["@lol-elo-charts/templates"],
+    noExternal: ["@lol-elo-charts/templates", "@lol-elo-charts/shared"],
 });
